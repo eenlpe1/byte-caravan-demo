c2e6f9773e075c1a251176bb891246c8bf595666
Emmanuel Enalpe III <emmanuelenalpe@gmail.com>
2025-10-02 14:07:31 +0800

    feat: add test button

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ export default function Home() {
     <main className="min-h-screen flex items-center justify-center bg-white text-neutral-900 dark:bg-neutral-950 dark:text-neutral-100 p-4">
       <div className="text-center space-y-4">
         <h1 className="text-3xl sm:text-4xl font-semibold tracking-tight">BYTE Campus Caravan - Legazpi</h1>
-        <p></p>
+
+        <button className="border border-blue-600 text-white p-2 rounded-md hover:bg-blue-600 m-2">Test Button</button>
       </div>
     </main>
   );
